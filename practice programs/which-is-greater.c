0e91ca63d99f8690216ef5832e044b35a68ebdbe
Ravneet Kaur <ravneetk730@gmail.com>
2018-09-06 02:28:56 +0000

    added comments to which-is-greater.c

--- practice programs/which-is-greater.c
+++ practice programs/which-is-greater.c
@@ -1,20 +1,23 @@
 #include<cs50.h>
 #include<stdio.h>
-
+/* This program tells you which number is bigger */
 int main(void)
 
 {
     int x = get_int("x:");
     int y = get_int("y:");
 
+        // if x is smaller
         if(x<y)
         {
             printf("x is less than y\n");
         }
+        // if x is greater
         else if(x>y)
         {
             printf("x is greater than y\n");
         }
+        // if x and y are equal
         else
         {
             printf("x is equal to y\n");
