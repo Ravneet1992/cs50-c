a8b1af36df171783d5c83d9599bcd87ff16fcf91
ravneet1992 <ravneetk730@gmail.com>
2018-09-06 03:16:15 +0000

    added one more program

---
+++ practice programs/positive-or-negative.c
@@ -0,0 +1,21 @@
+#include<cs50.h>
+#include<stdio.h>
+
+int main(void)
+{
+
+    int x = get_int("x:");
+
+    if(x>0)
+        {
+            printf("x is positive number\n");
+        }
+        else if (x<0)
+        {
+            printf("x is a negative number\n");
+        }
+        else
+        {
+            printf("x is neither positive nor negative\n");
+        }
+}
