1898ee82288ecc15b8cbf0c767f8061386ecf982
ravneet1992 <ravneetk730@gmail.com>
2018-09-08 20:13:09 +0000

    area and perimeter

---
+++ practice programs/perimeter.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void)
+{
+    int length = 7;
+    int width = 5;
+
+    printf("area of rectangle = %d square inches\n" , length*width);
+    printf("perimeter of rectangle = %d inches\n", 2*(length+width));
+}
