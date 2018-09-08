dc584f151d13ea14296b381be00135d41fde4440
ravneet1992 <ravneetk730@gmail.com>
2018-09-08 20:26:12 +0000

    perimeter and area of circle

---
+++ practice programs/perimeter_area_circle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void)
+{
+    int radius = 6;
+    float pi = 3.14;
+    printf("perimeter of circle = %.5f inches\n" , 2*pi*radius);
+    printf("area of circle = %.5f square inches\n", pi*radius*radius);
+
+}
