00992220b850f311f8bca5c8c147bacd8258b052
ravneet1992 <ravneetk730@gmail.com>
2018-09-13 01:12:21 +0000

    calculate distance b/w two points

---
+++ practice programs/distance.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main(void)
+
+{
+
+    int x1 = 25;
+    int x2 = 35;
+    int y1 = 15;
+    int y2 = 10;
+    int x =  (x1-x2);
+    int y =  (y1-y2);
+    printf("value of x = %d\n" ,x1-x2);
+    printf("value of y = %d\n" , y1-y2);
+    printf("distance between two points = %f\n", sqrt(x*x+y*y));
+
+
+
+
+
+}
