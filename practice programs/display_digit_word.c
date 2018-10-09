7892ce83a40f4401629983659e3076fc0bf25e71
ravneet1992 <ravneetk730@gmail.com>
2018-10-09 15:23:01 +0000

    display in words

---
+++ practice programs/display_digit_word.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<cs50.h>
+int main(void)
+{
+    int i = get_int("enter the digit:");
+    if(i==0)
+    printf("zero\n");
+    else if(i==1)
+    printf("One\n");
+
+    else if(i==2)
+    printf("two\n");
+
+     else if(i==3)
+    printf("three\n");
+
+     else if(i==4)
+    printf("four\n");
+
+     else if(i==5)
+    printf("five\n");
+
+     else if(i==6)
+    printf("six\n");
+
+     else if(i==7)
+    printf("seven\n");
+
+     else if(i==8)
+    printf("eight\n");
+
+     else if(i==9)
+    printf("nine\n");
+
+     else
+    printf("invalid digit..please try again..\n");
+}
+
+
+
+
+
