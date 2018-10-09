b438ca5acd1c814457d35b9b3a34d643a94d00f7
ravneet1992 <ravneetk730@gmail.com>
2018-10-09 16:20:00 +0000

    first ten natural numbers

---
+++ practice programs/natural_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+    int i;
+    printf("the first ten natural numbers are:\n");
+    for(i=1; i<=10; i++)
+    {
+    printf("%d ", i);
+    }
+printf("\n");
+
+}
